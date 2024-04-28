581a67343ec2d90aff71d3ab75a4df6703e36dc8
Vilaboo <5376386+vilaboo@users.noreply.github.com>
2024-04-29 04:28:16 +0800

    Update Base.py

--- Base.py
+++ Base.py
@@ -5,18 +5,21 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 import os 
 from dotenv import load_dotenv
+
 load_dotenv()
 db_file_path='FAISS_Index'
 embeddings = HuggingFaceEmbeddings()
+
 def creation_of_vectorDB_in_local(loader):
     data = loader.load()
-    db =FAISS.from_documents(data, embeddings)
+    db = FAISS.from_documents(data, embeddings)
     db.save_local(db_file_path)
-def creation_FAQ_chain():
-    db=FAISS.load_local(db_file_path, embeddings)
-    retriever =db.as_retriever(score_threshold=0.7)
 
-    llm = ChatGoogleGenerativeAI(model="gemini-pro",temperature=0.2)
+def creation_FAQ_chain():
+    db = FAISS.load_local(db_file_path, embeddings)
+    retriever = db.as_retriever(score_threshold=0.7)
+
+    llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.2)
 
     prompt_temp = """Use the document to answer the question. If you don't know the answer, just state "Unable to retrieve answer", don't try to make up an answer. Please return only the answer to the question and nothing else.
     Context: {}
@@ -24,7 +27,7 @@ def creation_FAQ_chain():
     Answer: ## Input your answer here ##
     """
 
-    PROMPT = PromptTemplate(template=prompt_temp, input_variables=["context", "question"])
+    PROMPT = PromptTemplate(template=prompt_temp, input_variables=["context", "question", "document_variable_name"])
     chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", 
                                         retriever=retriever, 
                                         input_key="query", 
