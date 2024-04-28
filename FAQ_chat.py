39cda6ba0851603698e9244a670c64fe22b7a76d
Vilaboo <5376386+vilaboo@users.noreply.github.com>
2024-04-28 21:08:44 +0800

    Update FAQ_chat.py

--- FAQ_chat.py
+++ FAQ_chat.py
@@ -1,6 +1,6 @@
-pip install streamlit --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org
-pip install deeplake --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org
-pip install google-generativeai --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org
+!pip install streamlit --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org
+!pip install deeplake --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org
+!pip install google-generativeai --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org
 
 import streamlit as st
 import os
