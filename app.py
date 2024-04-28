8208cbb1f55585abcb59e80e9a7fc16bff039ad4
Vilaboo <5376386+vilaboo@users.noreply.github.com>
2024-04-29 04:28:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,17 +14,17 @@ def main():
         st.title("Settings")
         st.markdown('---')
         st.subheader('Upload Your PDF File')
-        doc=st.file_uploader("Upload your PDF file and Click Process",'pdf')
+        doc = st.file_uploader("Upload your PDF file and Click Process", 'pdf')
         if st.button("Process"):
             with st.spinner("Processing"):
                 if doc is not None:
                     with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
                         tmp_file.write(doc.getvalue())
                         tmp_file_path = tmp_file.name
-            
+
                         st.success(f'File {doc.name} is successfully saved!')
                     
-                    load=pdf_loader(tmp_file_path)
+                    load = pdf_loader(tmp_file_path)
                     creation_of_vectorDB_in_local(load)
                     st.success("Process Done")
                 else:
@@ -33,20 +33,18 @@ def main():
     if "messages" not in st.session_state:
         st.session_state.messages = []
     for message in st.session_state.messages:
-        with st. chat_message(message["role"]):
-            st. markdown (message["content"])
-    query=st.chat_input("Ask the Question")
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+    query = st.chat_input("Ask the Question")
     if query:
-        ans=creation_FAQ_chain()
-        result=ans(query)
-        a=result["result"]
-        st. chat_message ("user"). markdown(query)
-        st. session_state. messages.append({"role": "user","content": query})
+        ans = creation_FAQ_chain()
+        result = ans(query)
+        a = result["result"]
+        st.chat_message("user").markdown(query)
+        st.session_state.messages.append({"role": "user", "content": query})
         with st.chat_message("assistant"):
-            st. markdown (a)
-            st. session_state.messages. append(
-                {"role": "assistant",
-                "content": a})
+            st.markdown(a)
+            st.session_state.messages.append({"role": "assistant", "content": a})
        
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
