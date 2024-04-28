7ed9aecfe63ae0ae4e5f2727f0ab1b274559c726
Vilaboo <5376386+vilaboo@users.noreply.github.com>
2024-04-29 03:30:16 +0800

    Update Base.py

--- Base.py
+++ Base.py
@@ -12,23 +12,22 @@ def creation_of_vectorDB_in_local(loader):
     data = loader.load()
     db =FAISS.from_documents(data, embeddings)
     db.save_local(db_file_path)
+def creation_FAQ_chain():
+    db=FAISS.load_local(db_file_path, embeddings)
+    retriever =db.as_retriever(score_threshold=0.7)
 
-def creation_FAQ_chain(pdf_content, user_question):
-    db = FAISS.load_local(db_file_path, embeddings)
-    retriever = db.as_retriever(score_threshold=0.7)
+    llm = ChatGoogleGenerativeAI(model="gemini-pro",temperature=0.2)
 
-    llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.2)
+    prompt_temp="""Given the following context and a question, generate an answer based on this context only.
+    In the answer try to provide as much text as possible from "response" section in the source document context without making much changes.
+    If the answer is not found in the context, kindly state "This Question not Present in My Database." Don't try to make up an answer.
+    CONTEXT: {context}
+    QUESTION: {question}"""
 
-    prompt_temp = """
-    Given the following context and a question, generate an answer based on the content of the uploaded PDF file.
-    CONTEXT:{pdf_content}
-    QUESTION:{user_question}
-    """
-
-    PROMPT = PromptTemplate(template=prompt_temp, input_variables=["pdf_content", "user_question"])
-    chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", 
+    PROMPT = PromptTemplate(template=prompt_temp, input_variables=["context", "question"])
+    chain = RetrievalQA.from_chain_type(llm=llm,chain_type="stuff", 
                                         retriever=retriever, 
                                         input_key="query", 
                                         return_source_documents=False,
-                                        chain_type_kwargs={"prompt": PROMPT})
+                                        chain_type_kwargs={"prompt" : PROMPT})
     return chain
